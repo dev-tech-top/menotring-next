c77657037ea10e418963cb86eec87504f1015d2c
dev-tech-top <stevebayard226@gmail.com>
2025-06-05 08:09:02 -0400

    insertt PPR tech

--- nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
+++ nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
@@ -1,20 +1,17 @@
 import { generateYAxis } from "@/app/lib/utils";
 import { CalendarIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
+import { fetchRevenue } from "@/app/lib/data";
 
 import { Revenue } from "@/app/lib/definitions";
-
 // This component is representational only.
 // For data visualization UI, check out:
 // https://www.tremor.so/
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue(); // Fetch data inside the component
   const chartHeight = 350;
 
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
